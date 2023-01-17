639787ee377537a1b56f71af9f162204979194f6
Angie Bermudez <angie.bermudez350@gmail.com>
2023-01-17 16:44:23 -0500

    Creando la similitud de una base de datos

--- MicroservicioApiSinDB/Startup.cs
+++ MicroservicioApiSinDB/Startup.cs
@@ -30,10 +30,7 @@ namespace MicroservicioApiSinDB
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
             });
         }
     }
